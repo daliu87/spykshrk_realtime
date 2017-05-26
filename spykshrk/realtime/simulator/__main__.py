9f56df433a1d74666ee6716237a4a2b40d917f5d
Daniel Liu <daliu87@gmail.com>
2017-05-25 20:00:18 -0700

    Switched profiler to yappi for multithreaded support

--- spykshrk/realtime/simulator/__main__.py
+++ spykshrk/realtime/simulator/__main__.py
@@ -10,6 +10,8 @@ import sys
 import os.path
 import getopt
 from mpi4py import MPI
+import yappi
+
 import time
 import json
 
@@ -84,42 +86,28 @@ def main(argv):
 
     # MPI node management
 
+    # Using yappi profiler for multithreading
+    if rank in config['rank_settings']['enable_profiler']:
+        yappi.start()
+
     if rank == config['rank']['supervisor']:
         # Supervisor node
-        if rank in config['rank_settings']['enable_profiler']:
-            main_proc = main_process.MainProcess(comm=comm, rank=rank, config=config)
-            cProfile.runctx('main_proc.main_loop()', globals=globals(), locals=locals(),
-                            filename=os.path.join(config['files']['output_dir'],
-                                                  config['files']['prefix'] +
-                                                  '.{:02d}.'.format(rank) +
-                                                  config['files']['profile_postfix']))
-        else:
-            main_proc = main_process.MainProcess(comm=comm, rank=rank, config=config)
-            main_proc.main_loop()
+        main_proc = main_process.MainProcess(comm=comm, rank=rank, config=config)
+        main_proc.main_loop()
 
     if rank in config['rank']['ripples']:
-        if rank in config['rank_settings']['enable_profiler']:
-            ripple_proc = ripple_process.RippleProcess(comm, rank, config=config)
-            cProfile.runctx('ripple_proc.main_loop()', globals=globals(), locals=locals(),
-                            filename=os.path.join(config['files']['output_dir'],
-                                                  config['files']['prefix'] +
-                                                  '.{:02d}.'.format(rank) +
-                                                  config['files']['profile_postfix']))
-        else:
-            ripple_proc = ripple_process.RippleProcess(comm, rank, config=config)
-            ripple_proc.main_loop()
+        ripple_proc = ripple_process.RippleProcess(comm, rank, config=config)
+        ripple_proc.main_loop()
 
     if rank == config['rank']['simulator']:
-        if rank in config['rank_settings']['enable_profiler']:
-            simulator_proc = simulator_process.SimulatorProcess(comm, rank, config=config)
-            cProfile.runctx('simulator_proc.main_loop()', globals=globals(), locals=locals(),
-                            filename=os.path.join(config['files']['output_dir'],
-                                                  config['files']['prefix'] +
-                                                  '.{:02d}.'.format(rank) +
-                                                  config['files']['profile_postfix']))
-        else:
-            simulator_proc = simulator_process.SimulatorProcess(comm, rank, config=config)
-            simulator_proc.main_loop()
+        simulator_proc = simulator_process.SimulatorProcess(comm, rank, config=config)
+        simulator_proc.main_loop()
 
-#cProfile.runctx('main(sys.argv[1:])', globals(), locals(), 'fsdatapy_profile')
+    if rank in config['rank_settings']['enable_profiler']:
+        yappi.stop()
+        yappi.get_func_stats().save(os.path.join(config['files']['output_dir'],
+                                                 config['files']['prefix'] +
+                                                 '.{:02d}.'.format(rank) +
+                                                 config['files']['profile_postfix']), type='pstat')
+
 main(sys.argv[1:])
