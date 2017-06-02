02df008b386458dd494313dc023545266d7878e6
Daniel Liu <daliu87@gmail.com>
2017-06-02 16:27:08 -0700

    Reorganized main rank to separate out send, recv and manager interfaces.

--- spykshrk/realtime/main_process.py
+++ spykshrk/realtime/main_process.py
@@ -21,6 +21,56 @@ import sys
 #     print('Warning: Attribute Error ({}), disabling IPython TerminalPdb.'.format(err))
 #     bp = lambda: None
 
+class MainProcess(realtime_process.RealtimeProcess):
+
+    def __init__(self, comm: MPI.Comm, rank, config):
+
+        self.comm = comm    # type: MPI.Comm
+        self.rank = rank
+        self.config = config
+
+        self.send_interface = MainMPISendInterface(comm=comm, rank=rank, config=config)
+        self.manager = MainSimulatorManager(rank=rank, config=config, parent=self, send_interface=self.send_interface)
+        self.recv_interface = MainSimulatorMPIRecvInterface(comm=comm, rank=rank,
+                                                            config=config, main_manager=self.manager)
+
+        super().__init__(comm=comm, rank=rank, config=config, ThreadClass=MainThread)
+
+        # TODO temporary measure to enable type hinting (typing.Generics is broken for PyCharm 2016.2.3)
+        self.thread = self.thread   # type: MainThread
+
+        self.terminate = False
+
+    def trigger_termination(self):
+        self.terminate = True
+
+    def main_loop(self):
+        self.thread.start()
+
+        while not self.terminate:
+            self.recv_interface.process_next_message()
+
+        self.class_log.info("Main Process Main reached end, exiting.")
+
+
+class MainThread(realtime_process.RealtimeThread):
+
+    def __init__(self, comm: MPI.Comm, rank, config, parent):
+        super().__init__(comm=comm, rank=rank, config=config, parent=parent)
+        ripple_ranks = self.config['rank']['ripples']
+
+        self._stop_next = False
+
+    def trigger_termination(self):
+        self._stop_next = True
+
+    def run(self):
+
+        while not self._stop_next:
+            pass
+
+        self.class_log.info("Main Process Thread reached end, exiting.")
+
 
 class StimDecider(realtime_process.RealtimeClass):
     def __init__(self, send_manager, ripple_n_above_thresh=sys.maxsize):
@@ -57,121 +107,149 @@ class StimDecider(realtime_process.RealtimeClass):
                 self._send_manager.start_stimulation()
 
 
-class MainProcess(realtime_process.RealtimeProcess):
-
+class MainMPISendInterface(realtime_process.RealtimeClass):
     def __init__(self, comm: MPI.Comm, rank, config):
-
-        self.comm = comm    # type: MPI.Comm
+        self.comm = comm
         self.rank = rank
         self.config = config
 
-        super().__init__(comm=comm, rank=rank, config=config, ThreadClass=MainThread)
+        super().__init__()
 
-        # TODO temporary measure to enable type hinting (typing.Generics is broken for PyCharm 2016.2.3)
-        self.thread = self.thread   # type: MainThread
+    def send_num_ntrode(self, rank, num_ntrodes):
+        self.class_log.debug("Sending number of ntrodes to rank {:}".format(rank))
+        self.comm.send(realtime_process.NumTrodesMessage(num_ntrodes), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
 
-        self.terminate = False
+    def send_channel_selection(self, rank, channel_selects):
+        self.comm.send(obj=ripple_process.ChannelSelection(channel_selects), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_new_writer_message(self, rank, new_writer_message):
+        self.comm.send(obj=new_writer_message, dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_start_rec_message(self, rank):
+        self.comm.send(obj=realtime_process.StartRecordMessage(), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_turn_on_datastreams(self, rank):
+        self.comm.send(obj=ripple_process.TurnOnDataStream(), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_ripple_parameter(self, rank, param_message):
+        self.comm.send(obj=param_message, dest=rank, tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_ripple_baseline_mean(self, rank, mean_dict):
+        self.comm.send(obj=ripple_process.CustomRippleBaselineMeanMessage(mean_dict=mean_dict), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def send_ripple_baseline_std(self, rank, std_dict):
+        self.comm.send(obj=ripple_process.CustomRippleBaselineStdMessage(std_dict=std_dict), dest=rank,
+                       tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+    def terminate_all(self):
+        terminate_ranks = list(range(self.comm.size))
+        terminate_ranks.remove(self.rank)
+        for rank in terminate_ranks:
+            self.comm.send(obj=realtime_process.TerminateMessage(), dest=rank,
+                           tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+
+class MainSimulatorManager(realtime_process.RealtimeClass):
+
+    def __init__(self, rank, config, parent: MainProcess, send_interface: MainMPISendInterface):
+
+        self.rank = rank
+        self.config = config
+        self.parent = parent
+        self.send_interface = send_interface
 
         self.rec_manager = binary_record.BinaryRecordsManager(manager_label='realtime_replay',
                                                               save_dir=self.config['files']['output_dir'],
                                                               file_prefix=self.config['files']['prefix'],
                                                               file_postfix=self.config['files']['rec_postfix'])
 
-    def trigger_termination(self):
-        self.terminate = True
-
-    def main_loop(self):
-        self.thread.start()
-        mpi_status = MPI.Status()
-
-        # Send ripple config to ripple ranks
-        rip_param_message = ripple_process.RippleParameterMessage(**self.config['ripple']['RippleParameterMessage'])
-        rip_baseline_mean_message = ripple_process. \
-            CustomRippleBaselineMeanMessage(dict(map(lambda x: (int(x[0]), x[1]),
-                                                     self.config['ripple']['CustomRippleBaselineMeanMessage'].items())))
-        rip_baseline_std_message = ripple_process. \
-            CustomRippleBaselineStdMessage(dict(map(lambda x: (int(x[0]), x[1]),
-                                                    self.config['ripple']['CustomRippleBaselineStdMessage'].items())))
+        super().__init__()
 
         for rip_rank in self.config['rank']['ripples']:
-            self.comm.send(obj=rip_param_message, dest=rip_rank,
-                           tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
-            self.comm.send(obj=rip_baseline_mean_message, dest=rip_rank,
-                           tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
-            self.comm.send(obj=rip_baseline_std_message, dest=rip_rank,
-                           tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
 
-        mpi_status = MPI.Status()
+            # Map json RippleParameterMessage onto python object and then send
+            rip_param_message = ripple_process.RippleParameterMessage(**self.config['ripple']['RippleParameterMessage'])
+            self.send_interface.send_ripple_parameter(rank=rip_rank, param_message=rip_param_message)
 
-        while not self.terminate:
-            message = self.comm.recv(status=mpi_status, tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+            # Convert json string keys into int (ntrode_id) and send
+            rip_mean_base_dict = dict(map(lambda x: (int(x[0]), x[1]),
+                                          self.config['ripple']['CustomRippleBaselineMeanMessage'].items()))
+            self.send_interface.send_ripple_baseline_mean(rank=rip_rank, mean_dict=rip_mean_base_dict)
 
-            if isinstance(message, simulator_process.SimTrodeListMessage):
-                self.class_log.debug("Received ntrode list from simulator {:}.".format(message.trode_list))
-                for rip_rank in self.config['rank']['ripples']:
-                    self.class_log.debug("Sending number of ntrodes to ripple rank {:}".format(rip_rank))
-                    self.comm.send(realtime_process.NumTrodesMessage(len(message.trode_list)), dest=rip_rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+            # Convert json string keys into int (ntrode_id) and send
+            rip_std_base_dict = dict(map(lambda x: (int(x[0]), x[1]),
+                                         self.config['ripple']['CustomRippleBaselineStdMessage'].items()))
+            self.send_interface.send_ripple_baseline_std(rank=rip_rank, std_dict=rip_std_base_dict)
 
-                # Round robin allocation of channels to ripple
-                enable_count = 0
-                all_ripple_process_enable = [[] for _ in self.config['rank']['ripples']]
-                for chan_ind, chan_id in enumerate(message.trode_list):
-                    all_ripple_process_enable[enable_count % len(self.config['rank']['ripples'])].append(chan_id)
-                    enable_count += 1
+    def handle_ntrode_list(self, trode_list):
+        self.class_log.debug("Received ntrode list from simulator {:}.".format(trode_list))
+        for rip_rank in self.config['rank']['ripples']:
+            self.send_interface.send_num_ntrode(rank=rip_rank, num_ntrodes=len(trode_list))
 
-                # Set channel assignments for all ripple ranks
-                for rank_ind, rank in enumerate(self.config['rank']['ripples']):
-                    self.comm.send(obj=ripple_process.ChannelSelection(all_ripple_process_enable[rank_ind]), dest=rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+        # Round robin allocation of channels to ripple
+        enable_count = 0
+        all_ripple_process_enable = [[] for _ in self.config['rank']['ripples']]
+        for chan_ind, chan_id in enumerate(trode_list):
+            all_ripple_process_enable[enable_count % len(self.config['rank']['ripples'])].append(chan_id)
+            enable_count += 1
 
-                # Update binary_record file writers before starting datastream
-                for rec_rank in self.config['rank_settings']['enable_rec']:
-                    self.comm.send(obj=self.rec_manager.new_writer_message(), dest=rec_rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
-                    self.comm.send(obj=realtime_process.StartRecordMessage(), dest=rec_rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+        # Set channel assignments for all ripple ranks
+        for rank_ind, rank in enumerate(self.config['rank']['ripples']):
+            self.send_interface.send_channel_selection(rank, all_ripple_process_enable[rank_ind])
 
-                sleep(0.5)
-                # Then turn on data streaming to ripple ranks
-                for rank_ind, rank in enumerate(self.config['rank']['ripples']):
-                    self.comm.send(obj=ripple_process.TurnOnDataStream(), dest=rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+        # Update binary_record file writers before starting datastream
+        for rec_rank in self.config['rank_settings']['enable_rec']:
+            self.send_interface.send_new_writer_message(rank=rec_rank,
+                                                        new_writer_message=self.rec_manager.new_writer_message())
 
-            elif isinstance(message, binary_record.BinaryRecordTypeMessage):
-                self.rec_manager.register_rec_type_message(message)
+            self.send_interface.send_start_rec_message(rank=rec_rank)
 
-            elif isinstance(message, realtime_process.TerminateMessage):
-                self.class_log.info('Received TerminateMessage from rank {:}, now terminating all.'.
-                                    format(mpi_status.source))
+        sleep(0.5)
+        # Then turn on data streaming to ripple ranks
+        for rank in self.config['rank']['ripples']:
+            self.send_interface.send_turn_on_datastreams(rank)
 
-                terminate_ranks = list(range(self.comm.size))
-                terminate_ranks.remove(self.rank)
-                for rank in terminate_ranks:
-                    self.comm.send(obj=realtime_process.TerminateMessage(), dest=rank,
-                                   tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
-
-                self.thread.trigger_termination()
-                self.trigger_termination()
-
-        self.class_log.info("Main Process Main reached end, exiting.")
-
-
-class MainThread(realtime_process.RealtimeThread):
-
-    def __init__(self, comm: MPI.Comm, rank, config, parent):
-        super().__init__(comm=comm, rank=rank, config=config, parent=parent)
-        ripple_ranks = self.config['rank']['ripples']
-
-        self._stop_next = False
+    def register_rec_type_message(self, message):
+        self.rec_manager.register_rec_type_message(message)
 
     def trigger_termination(self):
-        self._stop_next = True
+        self.send_interface.terminate_all()
 
-    def run(self):
+        self.parent.thread.trigger_termination()
+        self.parent.trigger_termination()
 
-        while not self._stop_next:
-            pass
 
-        self.class_log.info("Main Process Thread reached end, exiting.")
+class MainSimulatorMPIRecvInterface(realtime_process.RealtimeClass):
+
+    def __init__(self, comm: MPI.Comm, rank, config, main_manager: MainSimulatorManager):
+        self.comm = comm
+        self.rank = rank
+        self.config = config
+        self.main_manager = main_manager
+
+        self.mpi_status = MPI.Status()
+
+        super().__init__()
+
+    def process_next_message(self):
+        message = self.comm.recv(status=self.mpi_status, tag=realtime_process.MPIMessageTag.COMMAND_MESSAGE.value)
+
+        if isinstance(message, simulator_process.SimTrodeListMessage):
+            self.main_manager.handle_ntrode_list(message.trode_list)
+
+        elif isinstance(message, binary_record.BinaryRecordTypeMessage):
+            self.main_manager.register_rec_type_message(message)
+
+        elif isinstance(message, realtime_process.TerminateMessage):
+            self.class_log.info('Received TerminateMessage from rank {:}, now terminating all.'.
+                                format(self.mpi_status.source))
+
+            self.main_manager.trigger_termination()
+
 
