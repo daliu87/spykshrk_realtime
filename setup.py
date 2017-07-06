90ff8d20d4959181b617cb592d80f84354c443ec
Daniel Liu <daliu87@gmail.com>
2017-07-06 13:54:27 -0700

    Moved spykshrk package into it's own src folder.
    This is to keep running python in the root directory from using the
    local source, rather than in the installed site-package.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import numpy
 
 sourcefiles = ['RSTPython.pyx','RStarTree.c','RSTInterUtil.c','RSTInOut.c','RSTInstDel.c','RSTUtil.c','RSTQuery.c','RSTJoin.c','RSTFunctions.c']
 		
-sourcefiles = ['./spykshrk/realtime/rst/' + src for src in sourcefiles]
+sourcefiles = ['./src/spykshrk/realtime/rst/' + src for src in sourcefiles]
 
 extensions = [Extension('spykshrk.realtime.rst.RSTPython', sourcefiles, include_dirs=[numpy.get_include()])]
 
 setup (name = 'Spykshrk Realtime',
        ext_modules = cythonize(extensions),
-       packages=find_packages('.'),
-       package_dir={'':'.'})
+       packages=find_packages('./src/'),
+       package_dir={'':'./src/'})
