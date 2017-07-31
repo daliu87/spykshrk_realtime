7e0fb64eae6d944fda2870e2cdb8ad81503328ba
Daniel Liu <daliu87@gmail.com>
2017-07-31 16:27:43 -0700

    In rec_merge_hdf5 final merge and sort also writes out to a shared hdf5.

--- src/spykshrk/realtime/postprocessing/rec_merge_hdf5.py
+++ src/spykshrk/realtime/postprocessing/rec_merge_hdf5.py
@@ -7,18 +7,26 @@ import spykshrk.realtime.binary_record_cy as bin_rec_cy
 import multiprocessing as mp
 import multiprocessing.sharedctypes
 import pandas as pd
+import uuid
 import pickle
 import cProfile
 import time
 
+run_config = None
+hdf5_lock = None
 shared_mem = None
 shared_view = None
 
-def init_shared_mem(arr):
+
+def init_shared_mem(config, arr, hdf5_lock_local):
+    global run_config
     global shared_mem
     global shared_view
+    global hdf5_lock
+    run_config = config
     shared_mem = arr
     shared_view = memoryview(arr).cast('B')
+    hdf5_lock = hdf5_lock_local
 
 
 def binrec_to_pandas(binrec: bin_rec_cy.BinaryRecordsFileReader):
@@ -40,12 +48,22 @@ def merge_pandas(byte_range):
         merged.sort_values(['timestamp'], inplace=True)
         merged.reset_index(drop=True, inplace=True)
 
-    merged_bytes = pickle.dumps((rec_id, merged))
-    merged_bytes_size = len(merged_bytes)
+    hdf5_lock.acquire()
 
-    shared_view[byte_range[0]:(byte_range[0] + merged_bytes_size)] = merged_bytes
+    hdf5_filename = os.path.join(run_config['files']['output_dir'],
+                                 '{}.rec_merged.h5'.format(run_config['files']['prefix']))
 
-    return byte_range[0], (byte_range[0] + merged_bytes_size)
+    with pd.HDFStore(hdf5_filename, 'w') as hdf_store:
+        hdf_store['rec_{}'.format(rec_id)] = merged
+
+    hdf5_lock.release()
+
+    # merged_bytes = pickle.dumps((rec_id, merged))
+    # merged_bytes_size = len(merged_bytes)
+
+    # shared_view[byte_range[0]:(byte_range[0] + merged_bytes_size)] = merged_bytes
+
+    # return byte_range[0], (byte_range[0] + merged_bytes_size)
 
 
 def main(argv):
@@ -82,12 +100,14 @@ def main(argv):
             logging.warning('Binary record file not found, skipping: {}'.format(ex.filename))
 
     # Increase size for panda tables
-    total_bin_size = int(total_bin_size * 1.2)
+    total_bin_size = int(total_bin_size * 1.25)
 
     shared_arr = mp.sharedctypes.RawArray('B', total_bin_size)
     shared_arr_view = memoryview(shared_arr).cast('B')
 
-    p = mp.Pool(20, initializer=init_shared_mem, initargs=[shared_arr])
+    hdf5_lock_local = mp.Lock()
+
+    p = mp.Pool(20, initializer=init_shared_mem, initargs=[config, shared_arr, hdf5_lock_local], maxtasksperchild=1)
     logging.info("Converting binary record files into panda dataframes.")
     start_time = time.time()
     pandas_list = p.map(binrec_to_pandas, bin_list)
@@ -109,7 +129,6 @@ def main(argv):
     for pan_item in pandas_dict.items():
         pickled_pan_item = pickle.dumps(pan_item)
         pickled_pan_size = len(pickled_pan_item)
-
         shared_arr_view[start_byte:(start_byte+pickled_pan_size)] = pickled_pan_item
         byte_ranges.append((start_byte, start_byte + pickled_pan_size))
         # Set small gap between lists
@@ -120,34 +139,16 @@ def main(argv):
                  "shared memory, {:.01f} seconds ({:.02f} minutes).".format(end_time - start_time,
                                                                             (end_time - start_time)/60.))
 
-    logging.info("Merging and sorting each record type's dataframe.")
+    logging.info("Merging, sorting and saving each record type's dataframe.")
     start_time = time.time()
-    pandas_merged_address_ranges = p.map(merge_pandas, byte_ranges)
+    p.map(merge_pandas, byte_ranges)
+    #pandas_merged_address_ranges = p.map(merge_pandas, byte_ranges)
     end_time = time.time()
-    logging.info("Done merging and sorting all records,"
+    logging.info("Done merging and sorting and saving all records,"
                  " {:.01f} seconds ({:.02f} minutes).".format(end_time - start_time,
                                                               (end_time - start_time)/60.))
 
-    logging.info("Collecting dataframes and writing to HDF5 file.")
-    start_time = time.time()
 
-    pandas_merged = []
-    for byte_range in pandas_merged_address_ranges:
-        df = pickle.loads(shared_arr_view[byte_range[0]:byte_range[1]])
-        pandas_merged.append(df)
-
-    hdf_store = pd.HDFStore(os.path.join(config['files']['output_dir'],
-                                         '{}.rec_merged.h5'.format(config['files']['prefix'])))
-
-    for rec_id, rec_df in pandas_merged:
-        hdf_store['rec_{}'.format(rec_id)] = rec_df
-
-    hdf_store.close()
-
-    end_time = time.time()
-    logging.info("Done writing to HDF5 files, "
-                 "{:.01f} seconds ({:.02f} minutes).".format(end_time - start_time,
-                                                             (end_time - start_time)/60.))
 
 if __name__ == '__main__':
     cProfile.runctx('main(sys.argv[1:])', globals=globals(), locals=locals(), filename='pstats')
