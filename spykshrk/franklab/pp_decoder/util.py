a8c01f2c60d0c227a4b3f54f5925f517b6e90726
Daniel Liu <daliu87@gmail.com>
2018-01-19 15:40:43 -0800

    Updated ipyparallel and dask benchmark notebook with more examples.

--- spykshrk/franklab/pp_decoder/util.py
+++ spykshrk/franklab/pp_decoder/util.py
@@ -50,3 +50,34 @@ def simplify_pos_pandas(pos_data):
     return pos_data_notebook
 
 
+class Groupby:
+    def __init__(self, data, keys):
+        self.data = data
+        _, self.keys_as_int = np.unique(keys, return_inverse=True)
+        self.n_keys = max(self.keys_as_int)
+        self.set_indices()
+
+    def set_indices(self):
+        self.indices = [[] for i in range(self.n_keys+1)]
+        for i, k in enumerate(self.keys_as_int):
+            self.indices[k].append(i)
+        self.indices = [np.array(elt) for elt in self.indices]
+
+    def apply(self, function, vector, broadcast):
+        if broadcast:
+            result = np.zeros(len(vector))
+            for idx in self.indices:
+                result[idx] = function(vector[idx])
+        else:
+            result = np.zeros(self.n_keys)
+            for k, idx in enumerate(self.indices):
+                result[self.keys_as_int[k]] = function(vector[idx])
+
+        return result
+
+    def __iter__(self):
+        for inds in self.indices:
+            yield self.data[inds]
+
+
+
