Fixed no spike probability normalization.
Fixed resampling issue for data containers.

spykshrk/franklab/data_containers.py
@@ -416,7 +416,7 @@ class DayEpochTimeSeries(DayDataFrame):
             epoch = df.index.get_level_values('epoch')[0]
 
             new_timestamps = np.arange(df.index.get_level_values('timestamp')[0] +
-                                       (bin_size - df.index.get_level_values('timestamp')[0] % bin_size),
+                                       (df.index.get_level_values('timestamp')[0] % bin_size),
                                        df.index.get_level_values('timestamp')[-1] + 1, bin_size)
             new_times = new_timestamps / float(self.sampling_rate)
 
@@ -426,7 +426,7 @@ class DayEpochTimeSeries(DayDataFrame):
             # df.set_index(df.index.get_level_values('timestamp'), inplace=True)
             pos_data_bin_ids = np.arange(0, len(new_timestamps), 1)
 
-            pos_data_binned = df.loc[day, epoch].reindex(new_indices, method='ffill')
+            pos_data_binned = df.loc[day, epoch].reindex(new_indices).fillna(method='ffill')
             # pos_data_binned.set_index(new_timestamps)
             pos_data_binned['bin'] = pos_data_bin_ids
 
@@ -537,6 +537,7 @@ class EncodeSettings:
         self.arm_coordinates = encoder_config['position']['arm_pos']
         self.tetrodes = realtime_config[realtime_config['datasource']]['nspike_animal_info']['tetrodes']
 
+        self.pos_sampling_rate = encoder_config['position']['sampling_rate']
         self.pos_upper = encoder_config['position']['upper']
         self.pos_lower = encoder_config['position']['lower']
         self.pos_num_bins = encoder_config['position']['bins']

spykshrk/franklab/generator/place/pos_generator.py
@@ -13,37 +13,45 @@ class WtrackPosConstSimulator:
         self.trial_time = trial_time
         self.num_series = num_series
 
-        pos_epoch = np.array([])
-
         pos_series, pos_series_enum, pos_series_dir_enum = \
-                WtrackPosConstSimulator.simulate_series_const_speed(encode_settings.wtrack_arm_coordinates,
-                                                                    trial_time, encode_settings.sampling_rate)
+            WtrackPosConstSimulator.simulate_series_const_speed(encode_settings.wtrack_arm_coordinates,
+                                                                trial_time, encode_settings.pos_sampling_rate)
 
-        pos_epoch = np.append(pos_epoch, pos_series)
+        pos_series_timestamp = np.linspace(0, 4 * trial_time * encode_settings.sampling_rate,
+                                           4 * trial_time * encode_settings.pos_sampling_rate,
+                                           endpoint=False).astype('int')
 
-        pos_timestamp = np.arange(0, len(pos_epoch), 1)
+        pos_series_time = np.arange(0, pos_series.size / encode_settings.sampling_rate,
+                                    encode_settings.pos_sampling_rate / encode_settings.sampling_rate)
 
-        pos_time = np.arange(0, pos_epoch.size/encode_settings.sampling_rate, 1/encode_settings.sampling_rate)
+        pos_series_vel = np.ones(len(pos_series)) * ((pos_series[1] - pos_series[0]) /
+                                                     (pos_series_time[1] - pos_series_time[0]))
 
-        pos_vel = np.ones(len(pos_epoch)) * (pos_epoch[1] - pos_epoch[0]) / (pos_time[1] - pos_time[0])
+        pos_epoch = np.array([])
+        pos_timestamp = np.array([])
+        pos_time = np.array([])
+        pos_vel = np.array([])
+        pos_enum = np.array([])
+        pos_dir_enum = np.array([])
 
         # Duplicate
         num_dup = num_series - 1
-        for ii in range(num_dup):
-            pos_epoch = np.append(pos_epoch, pos_epoch)
-            pos_timestamp = np.arange(0, len(pos_epoch), 1)
-            pos_time = np.arange(0, pos_epoch.size/encode_settings.sampling_rate, 1/encode_settings.sampling_rate)
-            pos_vel = np.append(pos_vel, pos_vel)
-            pos_series_enum = np.append(pos_series_enum, pos_series_enum)
-            pos_series_dir_enum = np.append(pos_series_dir_enum, pos_series_dir_enum)
+        for ii in range(num_series):
+            pos_epoch = np.append(pos_epoch, pos_series)
+            pos_timestamp = np.append(pos_timestamp, pos_series_timestamp + ii * 4 * trial_time *
+                                      encode_settings.sampling_rate)
+            #pos_time = np.append(pos_time, pos_series_time + ii * trial_time)
+            pos_vel = np.append(pos_vel, pos_series_vel)
+            pos_enum = np.append(pos_enum, pos_series_enum)
+            pos_dir_enum = np.append(pos_dir_enum, pos_series_dir_enum)
 
         self.linpos_flat = FlatLinearPosition.from_numpy_single_epoch(1, 1, pos_timestamp, pos_epoch, pos_vel,
                                                                       encode_settings.sampling_rate,
                                                                       encode_settings.wtrack_arm_coordinates)
 
-        self.linpos_flat['arm'] = pd.Series(index=self.linpos_flat.index, data=pos_series_enum, dtype='category')
+        self.linpos_flat['arm'] = pd.Series(index=self.linpos_flat.index, data=pos_enum, dtype='category')
         self.linpos_flat['direction'] = pd.Series(index=self.linpos_flat.index,
-                                                  data=pos_series_dir_enum, dtype='category')
+                                                  data=pos_dir_enum, dtype='category')
 
     @staticmethod
     def simulate_arm_const_speed(arm_range, trial_time, trial_len, sampling_rate):

spykshrk/franklab/pp_decoder/pp_clusterless.py
@@ -199,13 +199,13 @@ class OfflinePPEncoder(object):
             enc_settings (EncodeSettings): Realtime encoding settings.
         Returns (np.array): The occupancy of the animal
         """
-        occupancy, occ_bin_edges = np.histogram(lin_obj[linflat_col_name], bins=enc_settings.pos_bin_edges,
-                                                density=True)
+        occupancy, occ_bin_edges = np.histogram(lin_obj[linflat_col_name], bins=enc_settings.pos_bin_edges)
         occupancy = np.convolve(occupancy, enc_settings.pos_kernel, mode='same')
 
         # occupancy
         occupancy = apply_no_anim_boundary(enc_settings.pos_bins, enc_settings.arm_coordinates, occupancy, np.nan)
-        occupancy += 1e-10
+        occupancy += 0.1
+        occupancy /= enc_settings.pos_sampling_rate
         return occupancy
 
     @staticmethod
@@ -223,7 +223,8 @@ class OfflinePPEncoder(object):
             firing_rate[fr_key] = np.convolve(firing_rate[fr_key], enc_settings.pos_kernel, mode='same')
             firing_rate[fr_key] = apply_no_anim_boundary(enc_settings.pos_bins, enc_settings.arm_coordinates,
                                                          firing_rate[fr_key])
-            firing_rate[fr_key] = firing_rate[fr_key] / (firing_rate[fr_key].sum() * enc_settings.pos_bin_delta)
+            #firing_rate[fr_key] = firing_rate[fr_key] / (firing_rate[fr_key].sum() * enc_settings.pos_bin_delta)
+            #firing_rate[fr_key] += 1
         return firing_rate
 
     @staticmethod

spykshrk/franklab/pp_decoder/wtrack_mapping.py
@@ -288,7 +288,7 @@ class WtrackLinposRecomposer(AttrDict):
             for tet_id, prob_no_spike_tet in prob_no_spike_cw.items():
                 prob_no_spike.setdefault(tet_id, np.zeros(encoding_settings.pos_num_bins))
                 prob_no_spike[tet_id][wtrack_decomposed.sel_data[rot_k]['main']['wtrack']['ind']] += \
-                         prob_no_spike_tet[wtrack_decomposed.sel_data[rot_k]['main']['decomposed']['ind']]
+                         prob_no_spike_tet[wtrack_decomposed.sel_data[rot_k]['main']['decomposed']['ind']] / 2
         return prob_no_spike
 
     @staticmethod
@@ -305,6 +305,7 @@ class WtrackLinposRecomposer(AttrDict):
                 trans_mat += WtrackLinposRecomposer._wtrack_recompose_trans_mat_part(rot_k, ord1, ord2,
                                                                                      eval('trans_mat_'+rot_k),
                                                                                      wtrack_decomposed, encode_settings)
+        trans_mat /= trans_mat.sum()
         return trans_mat
 
     @staticmethod